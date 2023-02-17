6f819d065ffb8ba3d4c3b01dc04c3f82f81072f9
Fernando Petrola <fernandop@sonatype.com>
2023-02-17 12:02:21 -0300

    adding charset test

---
+++ src/main/java/com/company/errorprone/tester/ErrorproneCharsetTest.java
@@ -0,0 +1,12 @@
+package com.company.errorprone.tester;
+
+public class ErrorproneCharsetTest {
+    
+    public static void main(String[] args) {
+
+        byte[] firstString= "example string".getBytes();
+        String result= new String(firstString);
+
+        System.out.println(result);
+    }
+}

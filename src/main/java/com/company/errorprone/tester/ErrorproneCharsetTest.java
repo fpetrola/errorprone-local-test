e4398667194774761e1762214bbdc8d2ef1554a9
Fernando Petrola <fpetrola@users.noreply.github.com>
2023-02-22 12:50:36 -0300

    Update ErrorproneCharsetTest.java

--- src/main/java/com/company/errorprone/tester/ErrorproneCharsetTest.java
+++ src/main/java/com/company/errorprone/tester/ErrorproneCharsetTest.java
@@ -4,7 +4,7 @@ public class ErrorproneCharsetTest {
     
     public static void main(String[] args) {
 
-        byte[] firstString= "example string".getBytes(Charset.defaultCharset());
+        byte[] firstString= "example string".getBytes();
         String result= new String(firstString);
 
         System.out.println(result);
